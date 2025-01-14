be3a909d47f72f2557aa6210045679053b6e2857
advl <700@779.mx>
2025-01-14 16:12:26 +0100

    Added domain tree structure

---
+++ apps/react/boilerplate-vite/src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import canonicalLogo from "/canonical.svg";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+import { Button } from "@canonical/react-ds-core";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a
+          href="https://canonical.com"
+          target="_blank"
+          referrerPolicy="no-referrer"
+          rel="noreferrer"
+        >
+          <img src={canonicalLogo} className="logo" alt="Canonical logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Canonical Design System</h1>
+      <h2>React Vite template</h2>
+      <div className="card">
+        <Button
+          label={`Count: ${count}`}
+          onClick={() => setCount((count) => count + 1)}
+        />
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
