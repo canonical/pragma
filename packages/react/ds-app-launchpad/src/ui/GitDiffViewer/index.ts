734ff680dbc98242fb7b9746a60c6165195d5975
Goulin Khoge <36013798+goulinkh@users.noreply.github.com>
2025-01-24 13:36:23 +0100

    export parser

--- packages/react/ds-app-launchpad/src/ui/GitDiffViewer/index.ts
+++ packages/react/ds-app-launchpad/src/ui/GitDiffViewer/index.ts
@@ -1,5 +1,6 @@
 /* @canonical/generator-canonical-ds 0.0.1 */
 export * from "./DiffLine/index.js";
 
+export { parseGitDiff } from "./diff-utils.js";
 export { default as GitDiffViewer } from "./GitDiffViewer.js";
 export * from "./types.js";
