bcde8f43360d4817a5c0bb3ceccdc31bd77553b6
advl <700@779.mx>
2025-02-11 20:53:57 +0100

    Linted files in .storybook

--- packages/react/ds-app-launchpad/.storybook/main.ts
+++ packages/react/ds-app-launchpad/.storybook/main.ts
@@ -3,7 +3,7 @@ import { createConfig } from "@canonical/storybook-config";
 import { dirname, join } from "node:path";
 
 function getAbsolutePath(value: string): string {
-	return dirname(require.resolve(join(value, "package.json")));
+  return dirname(require.resolve(join(value, "package.json")));
 }
 
 const config = createConfig(getAbsolutePath);
