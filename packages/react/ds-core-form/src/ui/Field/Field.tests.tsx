aaeeff7c66c870bd9f4f006172a44ed39b9f5868
advl <700@779.mx>
2025-02-28 19:30:21 +0100

    Added base field structure

--- packages/react/ds-core-form/src/ui/Field/Field.tests.tsx
+++ packages/react/ds-core-form/src/ui/Field/Field.tests.tsx
@@ -4,17 +4,17 @@ import Component from "./Field.js";
 import { InputType } from "./types.js";
 
 describe("Field component", () => {
-  // it("renders", () => {
-  //   render(<Component label={"Hello world!"} />);
-  //   expect(screen.getByText("Hello world!")).toBeInTheDocument();
-  // });
-  //
-  // it("applies className", () => {
-  //   render(<Component label={"Hello world!"} className="test-class" />);
-  //   expect(screen.getByText("Hello world!")).toHaveClass("test-class");
-  // });
-  // TODO add tests
-  it("passes", () => {
-    expect(true).toBe(true);
-  });
+	// it("renders", () => {
+	//   render(<Component label={"Hello world!"} />);
+	//   expect(screen.getByText("Hello world!")).toBeInTheDocument();
+	// });
+	//
+	// it("applies className", () => {
+	//   render(<Component label={"Hello world!"} className="test-class" />);
+	//   expect(screen.getByText("Hello world!")).toHaveClass("test-class");
+	// });
+	// TODO add tests
+	it("passes", () => {
+		expect(true).toBe(true);
+	});
 });
