f974b3f90c100d75ad3e5dca4c2475ac9871dca7
josetapadas <jose.alves@canonical.com>
2025-03-03 12:08:39 +0000

    fix: moved component to top folder

--- packages/react/ds-app-launchpad/src/ui/index.ts
+++ packages/react/ds-app-launchpad/src/ui/index.ts
@@ -1,2 +1,2 @@
 export * from "./GitDiffViewer/index.js";
-export { EditableBlock, type EditableBlockProps } from "./shared/EditableBlock/index.js"
+export { EditableBlock, type EditableBlockProps } from "./EditableBlock/index.js"
