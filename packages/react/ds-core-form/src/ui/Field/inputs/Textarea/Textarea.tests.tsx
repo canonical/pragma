795ca4ae91746f95f6b30371fccb413bdd9a2952
Julie Muzina <julie.muzina@canonical.com>
2025-03-05 10:11:36 -0500

    Update tests to pass

---
+++ packages/react/ds-core-form/src/ui/Field/inputs/Textarea/Textarea.tests.tsx
@@ -0,0 +1,13 @@
+/* @canonical/generator-ds 0.9.0-experimental.4 */
+
+import { render, screen } from "@testing-library/react";
+import * as decorators from "storybook/decorators.js";
+import { beforeEach, describe, expect, it } from "vitest";
+import Component from "./Textarea.js";
+
+describe("Textarea component", () => {
+  it("passes", () => {
+    // todo add real tests
+    expect(true).toBe(true);
+  });
+});
