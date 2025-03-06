eb1ab8914316d0acdc5bf74c0fae8ffc0ca0824d
josetapadas <jose.alves@canonical.com>
2025-03-06 09:10:36 +0000

    fix: fixed the wierd plural for the custom hook name

--- packages/react/ds-app-launchpad/src/ui/EditableBlock/hooks/useEditableBlock.ts
+++ packages/react/ds-app-launchpad/src/ui/EditableBlock/hooks/useEditableBlock.ts
@@ -2,10 +2,12 @@ import { useContext } from "react";
 import EditingContext from "../Context.js";
 import type { EditingContextType } from "../types.js";
 
-export const useEditableBlocks = (): EditingContextType => {
+export const useEditableBlock = (): EditingContextType => {
   const context = useContext(EditingContext);
   if (!context) {
     throw new Error("useEditing cannot be used directly.");
   }
   return context;
 };
+
+export default useEditableBlock;
