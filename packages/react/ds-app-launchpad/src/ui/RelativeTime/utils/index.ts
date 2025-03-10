0b148506b339f9bc37d47de205d50d748c72a085
Goulin Khoge <36013798+goulinkh@users.noreply.github.com>
2025-03-10 19:59:57 +0100

    rename file getOptimalUpdateInterval

--- packages/react/ds-app-launchpad/src/ui/RelativeTime/utils/index.ts
+++ packages/react/ds-app-launchpad/src/ui/RelativeTime/utils/index.ts
@@ -1,3 +1,3 @@
 export { default as formatHumanTime } from "./formatHumanTime.js";
+export { default as getOptimalUpdateInterval } from "./getOptimalUpdateInterval.js";
 export { default as parseInstant } from "./parseInstant.js";
-export { default as getOptimalUpdateInterval } from "./optimalUpdateInterval.js";
