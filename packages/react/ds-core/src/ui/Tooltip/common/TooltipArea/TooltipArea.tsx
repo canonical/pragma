97cde2b035dd5ab5523b42a5733a2402492ebeef
Julie Muzina <jmuzina@jmuzina.io>
2025-03-11 19:24:25 -0400

    Add note about tooltip portals

--- packages/react/ds-core/src/ui/Tooltip/common/TooltipArea/TooltipArea.tsx
+++ packages/react/ds-core/src/ui/Tooltip/common/TooltipArea/TooltipArea.tsx
@@ -81,7 +81,10 @@ const TooltipArea = ({
         Portal can only be rendered on the client
       */}
       {typeof window !== "undefined"
-        ? createPortal(TooltipMessageElement, parentElement || document.body)
+        ? // Portals allow the tooltip to be rendered outside the parent element
+          // This is helpful when the parent element is a scrollable container or has bounds that may be
+          // overflown by the tooltip message.
+          createPortal(TooltipMessageElement, parentElement || document.body)
         : TooltipMessageElement}
     </span>
   );
