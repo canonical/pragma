fbc23580421d053f2bdf4e1ebcfdd07f2151f5b7
advl <700@779.mx>
2025-03-20 19:45:50 +0100

    Fixed htmlFor in label not mapping properly

--- packages/react/ds-core-form/src/ui/Field/hooks/useFieldAriaProperties.ts
+++ packages/react/ds-core-form/src/ui/Field/hooks/useFieldAriaProperties.ts
@@ -7,30 +7,33 @@ import { useId, useMemo } from "react";
  * @returns An object containing ARIA attributes for input, label, description, and error state.
  */
 const useFieldAriaProps = (name: string, isError: boolean) => {
-  const uniqueId = useId();
-  const props = useMemo(() => {
-    const baseId = `${uniqueId}-${name}`;
-    const labelId = `${baseId}-label`;
-    const descriptionId = `${baseId}-description`;
-    const errorId = `${baseId}-error`;
+	const uniqueId = useId();
+	const props = useMemo(() => {
+		const baseId = `${uniqueId}-${name}`;
+		const labelId = `${baseId}-label`;
+		const descriptionId = `${baseId}-description`;
+		const errorId = `${baseId}-error`;
 
-    return {
-      input: {
-        id: baseId,
-        "aria-labelledby": labelId,
-        "aria-describedby": `${descriptionId}${isError}` ? ` ${labelId}` : "",
-        "aria-errormessage": errorId,
-        "aria-invalid": isError,
-      },
-      label: { id: labelId },
-      description: { id: descriptionId },
-      error: {
-        id: errorId,
-        // role: "alert",
-      },
-    };
-  }, [name, isError, uniqueId]);
-  return props;
+		return {
+			input: {
+				id: baseId,
+				"aria-labelledby": labelId,
+				"aria-describedby": `${descriptionId}${isError}` ? ` ${labelId}` : "",
+				"aria-errormessage": isError ? errorId : undefined,
+				"aria-invalid": isError,
+			},
+			label: {
+				id: labelId,
+				htmlFor: baseId,
+			},
+			description: { id: descriptionId },
+			error: {
+				id: errorId,
+				// role: "alert",
+			},
+		};
+	}, [name, isError, uniqueId]);
+	return props;
 };
 
 export default useFieldAriaProps;
