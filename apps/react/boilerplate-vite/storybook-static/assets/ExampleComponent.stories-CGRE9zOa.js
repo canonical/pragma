91d8288a4516b6025b92a1904dda6bc56d998fea
advl <700@779.mx>
2025-03-20 16:25:54 +0100

    Added storybook build files to gitignore

--- apps/react/boilerplate-vite/storybook-static/assets/ExampleComponent.stories-CGRE9zOa.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"./jsx-runtime-D_zvdyIk.js";const m="ds example-component",r=({id:a,children:p,className:c,style:i})=>o.jsx("div",{id:a,style:i,className:[m,c].filter(Boolean).join(" "),children:p});r.__docgenInfo={description:`description of the ExampleComponent component
-@returns {React.ReactElement} - Rendered ExampleComponent`,methods:[],displayName:"ExampleComponent",props:{id:{required:!1,tsType:{name:"string"},description:""},className:{required:!1,tsType:{name:"string"},description:""},children:{required:!1,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""},style:{required:!1,tsType:{name:"ReactCSSProperties",raw:"React.CSSProperties"},description:""}}};const d={title:"ExampleComponent",component:r},e={args:{children:o.jsx("span",{children:"Hello world!"})}};var n,s,t;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
-  args: {
-    children: <span>Hello world!</span>
-  }
-}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const u=["Default"];export{e as Default,u as __namedExportsOrder,d as default};
