4ac2c469f13ab51cd34905eeb133a5a4aa06e77e
advl <700@779.mx>
2025-03-21 00:01:32 +0100

    -

--- packages/react/ds-core-form/src/ui/Field/inputs/SimpleChoices/types.ts
+++ packages/react/ds-core-form/src/ui/Field/inputs/SimpleChoices/types.ts
@@ -1,22 +1,24 @@
+import type { FieldValues, UseFormRegister } from "react-hook-form";
 /* @canonical/generator-ds 0.9.0-experimental.9 */
 import type { BaseInputProps, OptionsProps } from "../types.js";
 
-export type OptionProps = {
-  name: string;
-  type: string;
-  value: string;
-  label: string;
-  // TODO, a function
-  register: unknown;
-  registerProps?: Record<string, unknown>; //TODO improve
-  disabled: boolean;
+export type OptionProps<TFieldValues extends FieldValues> = {
+	name: string;
+	type: string;
+	value: string;
+	label: string;
+	// TODO, a function
+	// register: UseFormRegister<TFieldValues>;
+	register: UseFormRegister<Record<string, unknown>>;
+	registerProps?: Record<string, unknown>; //TODO improve
+	disabled: boolean;
 };
 
 export type SimpleChoicesProps = BaseInputProps &
-  OptionsProps & {
-    /** Whether the select should allow multiple selections. Is enabled, will be represented as a set of checkboxes, otherwise, radios */
-    isMultiple?: boolean;
+	OptionsProps & {
+		/** Whether the select should allow multiple selections. Is enabled, will be represented as a set of checkboxes, otherwise, radios */
+		isMultiple?: boolean;
 
-    /** Whether the input is disabled */
-    disabled?: boolean;
-  };
+		/** Whether the input is disabled */
+		disabled?: boolean;
+	};
