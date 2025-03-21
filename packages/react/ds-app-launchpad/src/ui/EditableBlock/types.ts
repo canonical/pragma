422d217ba2be486374eed3348892340bb38f9264
Goulin Khoge <36013798+goulinkh@users.noreply.github.com>
2025-03-21 13:24:02 +0100

    feat: add readonly mode to the editable block component

--- packages/react/ds-app-launchpad/src/ui/EditableBlock/types.ts
+++ packages/react/ds-app-launchpad/src/ui/EditableBlock/types.ts
@@ -26,6 +26,9 @@ export interface EditableBlockProps<T extends EditElementProps> {
   title?: string;
   /** HTML element type to render as the title component (e.g., 'div', 'section') */
   tag?: keyof JSX.IntrinsicElements;
+
+  /** Whether the block is read-only */
+  readonly?: boolean;
 }
 
 export default EditableBlockProps;
