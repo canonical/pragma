9769185885035a46cc50e7e005ee93e92c469d12
advl <700@779.mx>
2025-03-26 21:47:53 +0100

    Type

--- packages/react/ds-core-form/src/ui/Field/inputs/Combobox/utils/convertValueToItem.ts
+++ packages/react/ds-core-form/src/ui/Field/inputs/Combobox/utils/convertValueToItem.ts
@@ -2,11 +2,11 @@ import type { Option } from "../../../types.js";
 import { VALUE_KEY } from "../constants.js";
 
 function convertValueToItem(
-	value: string,
-	items: Option[],
-	valueKey: keyof Option = VALUE_KEY,
+  value: string,
+  items: Option[],
+  valueKey: keyof Option = VALUE_KEY,
 ): Option | null {
-	return items.find((item: Option) => item[valueKey] === value) || null;
+  return items.find((item: Option) => item[valueKey] === value) || null;
 }
 
 export default convertValueToItem;
