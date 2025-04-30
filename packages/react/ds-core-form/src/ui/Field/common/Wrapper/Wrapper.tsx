27c7f67799c34552f9d1614c65edc8347fdcf63e
advl <700@779.mx>
2025-04-30 22:22:03 +0200

    States, enhanced

--- packages/react/ds-core-form/src/ui/Field/common/Wrapper/Wrapper.tsx
+++ packages/react/ds-core-form/src/ui/Field/common/Wrapper/Wrapper.tsx
@@ -3,6 +3,7 @@ import type React from "react";
 import { useMemo } from "react";
 import type { BaseInputProps, WrapperProps } from "../../types.js";
 import "./styles.css";
+import { states } from "ui/constants.js";
 import { Description, Error as FieldError, Label } from "../index.js";
 import { useFieldWrapper } from "./hooks/index.js";
 
@@ -13,69 +14,71 @@ const componentCssClassName = "ds form-wrapper";
  * @returns {React.ReactElement} - Rendered Wrapper
  */
 const Wrapper = <ComponentProps extends BaseInputProps>({
-  id,
-  className,
-  style,
+	id,
+	className,
+	style,
 
-  name,
-  Component,
-  description,
-  label,
-  isOptional,
-  registerProps: userRegisterProps,
-  nestedRegisterProps,
-  unregisterOnUnmount,
+	name,
+	Component,
+	description,
+	label,
+	isOptional,
+	registerProps: userRegisterProps,
+	nestedRegisterProps,
+	unregisterOnUnmount,
 
-  mockLabel = false,
-  ...otherProps
+	mockLabel = false,
+	...otherProps
 }: WrapperProps<ComponentProps>): React.ReactElement => {
-  const { fieldError, isError, ariaProps, registerProps } = useFieldWrapper(
-    name,
-    {
-      label,
-      isOptional,
-      userRegisterProps,
-      nestedRegisterProps,
-      unregisterOnUnmount,
-    },
-  );
+	const { fieldError, isError, ariaProps, registerProps } = useFieldWrapper(
+		name,
+		{
+			label,
+			isOptional,
+			userRegisterProps,
+			nestedRegisterProps,
+			unregisterOnUnmount,
+		},
+	);
 
-  // biome-ignore lint/correctness/useExhaustiveDependencies: -
-  const componentProps = useMemo(
-    () => ({
-      name,
-      registerProps,
-      ...ariaProps.input,
-      ...otherProps,
-    }),
-    [name, registerProps, ariaProps.input],
-  ) as unknown as ComponentProps;
+	// biome-ignore lint/correctness/useExhaustiveDependencies: -
+	const componentProps = useMemo(
+		() => ({
+			name,
+			registerProps,
+			...ariaProps.input,
+			...otherProps,
+		}),
+		[name, registerProps, ariaProps.input],
+	) as unknown as ComponentProps;
 
-  return (
-    <div
-      id={id}
-      style={style}
-      className={[componentCssClassName, className].filter(Boolean).join(" ")}
-    >
-      <Label
-        name={name}
-        isOptional={isOptional}
-        tag={mockLabel ? "legend" : undefined}
-        {...ariaProps.label}
-      >
-        {label}
-      </Label>
-      <div className="payload">
-        <Description {...ariaProps.description}>{description}</Description>
-        <Component {...componentProps} />
-        {isError && (
-          <FieldError {...ariaProps.error}>
-            {fieldError?.message?.toString()}
-          </FieldError>
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div
+			id={id}
+			style={style}
+			className={[componentCssClassName, className, isError && states.Danger]
+				.filter(Boolean)
+				.join(" ")}
+		>
+			<Label
+				name={name}
+				isOptional={isOptional}
+				tag={mockLabel ? "legend" : undefined}
+				{...ariaProps.label}
+			>
+				{label}
+			</Label>
+			<div className="payload">
+				<Description {...ariaProps.description}>{description}</Description>
+				<Component {...componentProps} />
+				{isError && (
+					<FieldError {...ariaProps.error}>
+						{fieldError?.message?.toString()}
+					</FieldError>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default Wrapper;
