eb1cce8d736c7ac560249730b1725c25808af0f0
Julie Muzina <julie.muzina@canonical.com>
2025-05-01 10:29:25 -0400

    rapid succession test 1/2 - this should be cancelled by next commit

--- packages/react/ds-core/src/ui/Button/Button.tsx
+++ packages/react/ds-core/src/ui/Button/Button.tsx
@@ -13,7 +13,7 @@ const Button = ({
   return (
     <button
       id={id}
-      className={["ds", "buttonnn", appearance, className]
+      className={["ds", "buttonnnn", appearance, className]
         .filter(Boolean)
         .join(" ")}
       style={style}
