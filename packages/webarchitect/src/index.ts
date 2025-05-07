1087a72dbc17d0754fe5fd6af484ae1a193bb731
advl <700@779.mx>
2025-05-07 14:31:57 +0200

    cleanup

--- packages/webarchitect/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as casing } from "./casing.js";
-export { default as debounce } from "./debounce.js";
-export { default as invariant } from "./invariant.js";
-export { default as throttle } from "./throttle.js";
-export type { AllOrNone } from "./types.js";
