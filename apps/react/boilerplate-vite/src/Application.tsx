f5168a7f32c98389b6e448893ba534fa3f70fee6
advl <700@779.mx>
2025-05-20 11:24:13 +0200

    Wired boilerplate with grid

--- apps/react/boilerplate-vite/src/Application.tsx
+++ apps/react/boilerplate-vite/src/Application.tsx
@@ -5,51 +5,60 @@ import reactLogo from "./assets/react.svg";
 import "./Application.css";
 
 const LazyButton = lazy(
-  () =>
-    new Promise((resolve) => {
-      // @ts-ignore
-      setTimeout(() => resolve(import("./LazyComponent.js")), 2000);
-    }),
+	() =>
+		new Promise((resolve) => {
+			// @ts-ignore
+			setTimeout(() => resolve(import("./LazyComponent.js")), 2000);
+		}),
 );
 
 function App() {
-  const [count, setCount] = useState(0);
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a
-          href="https://canonical.com"
-          target="_blank"
-          referrerPolicy="no-referrer"
-          rel="noreferrer"
-        >
-          <img src={canonicalLogo} className="logo" alt="Canonical logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Canonical Design System</h1>
-      <h2>React Vite template</h2>
-      <Suspense fallback={"Loading..."}>
-        <LazyButton />
-      </Suspense>
-      <div className="card">
-        <TooltipArea
-          preferredDirections={["right", "bottom"]}
-          Message={`Increment count to ${count + 1}`}
-        >
-          <Button onClick={() => setCount((count) => count + 1)}>
-            Count: {count}
-          </Button>
-        </TooltipArea>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<section>
+				<h1 className="row">Canonical Design System</h1>
+				<h2 className="row">React Vite template</h2>
+				<a
+					href="https://canonical.com"
+					target="_blank"
+					referrerPolicy="no-referrer"
+					rel="noreferrer"
+					style={{
+						gridColumn: "1/3",
+					}}
+				>
+					<img src={canonicalLogo} className="logo" alt="Canonical logo" />
+				</a>
+				<a href="https://react.dev" target="_blank" rel="noreferrer">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</section>
+			<section>
+				<div className="row">
+					<Suspense fallback={"Loading..."}>
+						<LazyButton />
+					</Suspense>
+				</div>
+			</section>
+			<section>
+				<div className="card" style={{ gridColumn: "1/4" }}>
+					<TooltipArea
+						preferredDirections={["right", "bottom"]}
+						Message={`Increment count to ${count + 1}`}
+					>
+						<Button onClick={() => setCount((count) => count + 1)}>
+							Count: {count}
+						</Button>
+					</TooltipArea>
+					<p>
+						Edit <code>src/App.tsx</code> and save to test HMR
+					</p>
+				</div>
+			</section>
+		</>
+	);
 }
 
 export default App;
