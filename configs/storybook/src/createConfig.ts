349474d2beddfc28776c393e58bc789c1adfda91
Goulin Khoge <36013798+goulinkh@users.noreply.github.com>
2025-06-06 16:02:25 +0200

    fix(storybook): Refactor Storybook configuration for Svelte support

--- configs/storybook/src/createConfig.ts
+++ configs/storybook/src/createConfig.ts
@@ -1,5 +1,5 @@
-import type { StorybookConfig } from "@storybook/react-vite";
 import { dirname, join } from "node:path";
+import type { StorybookConfig } from "@storybook/react-vite";
 
 function getAbsolutePath(value: string): string {
   return dirname(require.resolve(join(value, "package.json")));
@@ -14,18 +14,20 @@ const FRAMEWORK_CONFIGS = {
   svelte: {
     storyPatterns: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx|svelte)"],
     additionalAddons: [
+      // This Storybook addon allows you to write Storybook stories using the Svelte language instead of ESM that regular CSF is based on.
       getAbsolutePath("@storybook/addon-svelte-csf"),
-      getAbsolutePath("@storybook/addon-a11y"),
     ],
     frameworkName: getAbsolutePath("@storybook/sveltekit"),
   },
 } as const;
 
 const BASE_ADDONS = [
-  getAbsolutePath("@storybook/addon-links"),
-  getAbsolutePath("@storybook/addon-essentials"),
   getAbsolutePath("@chromatic-com/storybook"),
+  // provides accessibility testing for Storybook stories. It uses axe-core to run the tests.
+  getAbsolutePath("@storybook/addon-a11y"),
+  getAbsolutePath("@storybook/addon-essentials"),
   getAbsolutePath("@storybook/addon-interactions"),
+  getAbsolutePath("@storybook/addon-links"),
   getAbsolutePath("@storybook/addon-themes"),
   // This is a bit weird, but for some reason this doesn't work when referenced via getAbsolutePath
   // see also: https://github.com/storybookjs/storybook/issues/24351#issuecomment-1777911065
@@ -44,7 +46,7 @@ function createConfig(options: CreateConfigOptions = {}): StorybookConfig {
 
   if (!frameworkConfig) {
     throw new Error(
-      `Unsupported framework: ${framework}. Supported frameworks: ${Object.keys(FRAMEWORK_CONFIGS).join(", ")}`
+      `Unsupported framework: ${framework}. Supported frameworks: ${Object.keys(FRAMEWORK_CONFIGS).join(", ")}`,
     );
   }
 
