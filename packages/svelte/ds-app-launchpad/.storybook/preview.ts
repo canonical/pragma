dda556212ac3eb6402ea8d243010c85ec188190e
advl <700@779.mx>
2025-06-12 20:06:39 +0200

    Disabled the theming addon for now

--- packages/svelte/ds-app-launchpad/.storybook/preview.ts
+++ packages/svelte/ds-app-launchpad/.storybook/preview.ts
@@ -1,19 +1,19 @@
-import { withThemeByClassName } from "@storybook/addon-themes";
+// import { withThemeByClassName } from "@storybook/addon-themes";
 import type { Preview } from "@storybook/react";
 
 import "@canonical/styles-debug/baseline-grid";
 
 const preview: Preview = {
-  decorators: [
-    withThemeByClassName({
-      themes: {
-        light: "is-light",
-        dark: "is-dark",
-        paper: "is-paper",
-      },
-      defaultTheme: "light",
-    }),
-  ],
+	decorators: [
+		// withThemeByClassName({
+		//   themes: {
+		//     light: "is-light",
+		//     dark: "is-dark",
+		//     paper: "is-paper",
+		//   },
+		//   defaultTheme: "light",
+		// }),
+	],
 };
 
 export default preview;
