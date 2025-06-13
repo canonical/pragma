256b8e80bfadb38cd1f58bb553fd265e4a7d581e
advl <700@779.mx>
2025-06-13 17:13:57 +0200

    Reenabled theme addon + removed hackfix spreading configs

--- configs/storybook/src/createConfig.ts
+++ configs/storybook/src/createConfig.ts
@@ -3,10 +3,11 @@ import type { StorybookConfig } from "@storybook/react-vite";
 import { dirname, join } from "node:path";
 
 function getAbsolutePath(value: string): string {
-  return dirname(require.resolve(join(value, "package.json")));
+	return dirname(require.resolve(join(value, "package.json")));
 }
 
 type CreateConfigOptions = {
+<<<<<<< HEAD
   staticDirs?: string[];
   extraAddons?: string[];
   disabledAddons?: string[];
@@ -40,6 +41,41 @@ const createConfig = (options: CreateConfigOptions = {}): StorybookConfig => ({
     check: true,
   },
   staticDirs: options.staticDirs,
+=======
+	staticDirs?: string[];
+	extraAddons?: string[];
+};
+
+const createConfig = (options: CreateConfigOptions = {}): StorybookConfig => ({
+	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+	addons: [
+		getAbsolutePath("@chromatic-com/storybook"),
+		getAbsolutePath("@storybook/addon-docs"),
+		getAbsolutePath("@storybook/addon-a11y"),
+		getAbsolutePath("@storybook/addon-vitest"),
+		getAbsolutePath("@storybook/addon-themes"),
+		// // This is a bit weird, but for some reason this doesn't work when referenced via getAbsolutePath
+		// // see also: https://github.com/storybookjs/storybook/issues/24351#issuecomment-1777911065
+		"@canonical/storybook-addon-baseline-grid",
+		...(options.extraAddons || []),
+		// getAbsolutePath("@storybook/addon-links"),
+		// getAbsolutePath("@storybook/addon-essentials"),
+		// getAbsolutePath("@chromatic-com/storybook"),
+		// getAbsolutePath("@storybook/addon-interactions"),
+		// getAbsolutePath("@storybook/addon-themes"),
+	],
+	framework: {
+		name: getAbsolutePath("@storybook/react-vite"),
+		options: {},
+	},
+	core: {
+		disableTelemetry: true,
+	},
+	typescript: {
+		check: true,
+	},
+	staticDirs: options.staticDirs,
+>>>>>>> a461d54 (Reenabled theme addon + removed hackfix spreading configs)
 });
 
 export default createConfig;
--- packages/react/ds-app-stores/.storybook/main.ts
+++ packages/react/ds-app-stores/.storybook/main.ts
@@ -1,10 +1,7 @@
 import { createConfig } from "@canonical/storybook-config";
 
 const config = createConfig({
-  staticDirs: ["../src/assets", "../public"],
+	staticDirs: ["../src/assets", "../public"],
 });
 
-/* Otherwise leads to a TS error "CSF Parsing error: Expected 'ObjectExpression' but found 'CallExpression' instead in 'CallExpression'."
- * https://github.com/storybookjs/storybook/issues/26778#issuecomment-2584041985
- */
-export default { ...config };
+export default config;
