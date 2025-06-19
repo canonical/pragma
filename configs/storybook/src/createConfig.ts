aa75e17fb3459f0667cb296a403d15b41a6ea0a3
advl <700@779.mx>
2025-06-19 23:50:28 +0200

    -

--- configs/storybook/src/createConfig.ts
+++ configs/storybook/src/createConfig.ts
@@ -2,38 +2,38 @@ import { dirname, join } from "node:path";
 import type { StorybookConfig } from "@storybook/react-vite";
 
 function getAbsolutePath(value: string): string {
-	return dirname(require.resolve(join(value, "package.json")));
+  return dirname(require.resolve(join(value, "package.json")));
 }
 
 type CreateConfigOptions = {
-	staticDirs?: string[];
-	extraAddons?: string[];
-	disabledAddons?: string[];
+  staticDirs?: string[];
+  extraAddons?: string[];
+  disabledAddons?: string[];
 };
 
 const createConfig = (options: CreateConfigOptions = {}): StorybookConfig => ({
-	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-	addons: [
-		getAbsolutePath("@chromatic-com/storybook"),
-		getAbsolutePath("@storybook/addon-docs"),
-		getAbsolutePath("@storybook/addon-a11y"),
-		getAbsolutePath("@storybook/addon-vitest"),
-		// // This is a bit weird, but for some reason this doesn't work when referenced via getAbsolutePath
-		// // see also: https://github.com/storybookjs/storybook/issues/24351#issuecomment-1777911065
-		"@canonical/storybook-addon-baseline-grid",
-		...(options.extraAddons || []),
-	].filter((addon) => !options.disabledAddons?.includes(addon)),
-	framework: {
-		name: getAbsolutePath("@storybook/react-vite"),
-		options: {},
-	},
-	core: {
-		disableTelemetry: true,
-	},
-	typescript: {
-		check: true,
-	},
-	staticDirs: options.staticDirs,
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    getAbsolutePath("@chromatic-com/storybook"),
+    getAbsolutePath("@storybook/addon-docs"),
+    getAbsolutePath("@storybook/addon-a11y"),
+    getAbsolutePath("@storybook/addon-vitest"),
+    // // This is a bit weird, but for some reason this doesn't work when referenced via getAbsolutePath
+    // // see also: https://github.com/storybookjs/storybook/issues/24351#issuecomment-1777911065
+    "@canonical/storybook-addon-baseline-grid",
+    ...(options.extraAddons || []),
+  ].filter((addon) => !options.disabledAddons?.includes(addon)),
+  framework: {
+    name: getAbsolutePath("@storybook/react-vite"),
+    options: {},
+  },
+  core: {
+    disableTelemetry: true,
+  },
+  typescript: {
+    check: true,
+  },
+  staticDirs: options.staticDirs,
 });
 
 export default createConfig;
