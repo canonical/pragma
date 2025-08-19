d1ade128955ade65f58c2bd7d48b1c2281a6763b
Julie Muzina <jmuzina@jmuzina.io>
2025-08-19 13:38:08 -0400

    Add math utils

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,5 +1,6 @@
 export { default as casing } from "./casing.js";
 export { default as debounce } from "./debounce.js";
 export { default as invariant } from "./invariant.js";
+export * as math from "./math/index.js";
 export { default as throttle } from "./throttle.js";
 export type { AllOrNone } from "./types.js";
