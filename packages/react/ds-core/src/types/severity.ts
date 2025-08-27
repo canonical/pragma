d1359dfc848bbc8f684f4d68f8b558983aa74739
Julie Muzina <jmuzina@pm.me>
2025-08-27 19:27:34 -0400

    Badge uses global intents for severity appearance instead of local modifiers

--- packages/react/ds-core/src/types/severity.ts
+++ packages/react/ds-core/src/types/severity.ts
@@ -2,7 +2,7 @@
  * TODO this should probably be moved to a non-React-specific package, tbd in code review
  */
 export type Severity =
-  | "default"
+  | "neutral"
   | "positive"
   | "negative"
   | "caution"
