5165e5afd0c1ce08be90d87ebe365ae2c6c2c1d0
Julie Muzina <jmuzina@pm.me>
2025-09-11 00:28:00 -0400

    build Section component

--- packages/react/ds-core/src/ui/index.ts
+++ packages/react/ds-core/src/ui/index.ts
@@ -4,4 +4,5 @@ export * from "./Chip/index.js";
 export * from "./Icon/index.js";
 export * from "./Link/index.js";
 export * from "./Rule/index.js";
+export * from "./Section/index.js";
 export * from "./Tooltip/index.js";
