4b518f1e398bf8b1e8c5ec68bb7b7516ff5ff641
Julie Muzina <jmuzina@pm.me>
2025-09-12 00:33:16 -0400

    add link `as` per suggestion from @advl

--- packages/react/ds-core/src/ui/Link/Link.tsx
+++ packages/react/ds-core/src/ui/Link/Link.tsx
@@ -1,6 +1,6 @@
 /* @canonical/generator-ds 0.10.0-experimental.2 */
 
-import type React from "react";
+import type { ReactElement } from "react";
 import { useMemo } from "react";
 import type { LinkProps } from "./types.js";
 import "./styles.css";
@@ -8,25 +8,25 @@ import "./styles.css";
 const componentCssClassName = "ds link";
 
 /**
- * React implementation of the Link (anchor element)
- * @returns {React.ReactElement} - Rendered Link
+ * Link component that can be rendered as different elements while maintaining consistent styling
  * @implements syntax:core:component:link:1.0.0
  */
 const Link = ({
+  as: Wrapper = "a",
   className,
   children,
   appearance: appearanceProp,
   activationContents,
   ...props
-}: LinkProps): React.ReactElement => {
-  // If hoverContents is provided, the link should be rendered as a soft link
+}: LinkProps): ReactElement => {
+  // If activationContents is provided, the link should be rendered as a soft link
   const appearance = useMemo(
     () => (activationContents ? "soft" : appearanceProp),
     [appearanceProp, activationContents],
   );
 
   return (
-    <a
+    <Wrapper
       className={[componentCssClassName, className, appearance]
         .filter(Boolean)
         .join(" ")}
@@ -36,8 +36,10 @@ const Link = ({
       {activationContents && (
         <span className="activation-contents">{activationContents}</span>
       )}
-    </a>
+    </Wrapper>
   );
 };
+
+Link.displayName = "Link";
 
 export default Link;
