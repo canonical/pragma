3dc9c4632d41284f973795c783446e8e02e47e6b
Julie Muzina <julie.muzina@canonical.com>
2025-10-09 11:33:32 -0400

    fix(React Core): Remove/rename unneeded icons (#354)
    * prune icons per https://github.com/canonical/pragma/issues/333#issuecomment-3364617394
    
    * chromatic apps/packages should also re-test on ds-assets updates
    
    * add back `timed-out` icon per design
    
    * rename `contact` icon `email`; remove previous email social icon
    
    * rm `copy-to-clipboard`, `topic`
    
    * email icon uses currentColor
    
    * fix `download` icon not being visible (wrong svg g path) - also add tests to ensure groupname-filename match
    
    * address https://github.com/canonical/pragma/pull/354#issuecomment-3381138505 by @diana-stanciulescu
    
    * rm accidental standards files from another pr
    
    * rename to `menu-contextual` and `user-profile`

--- packages/ds-assets/vitest.config.ts
+++ packages/ds-assets/vitest.config.ts
@@ -2,20 +2,12 @@ import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "./vite.config.js";
 
 export default mergeConfig(
-  // Base the test config on the base vite config
   viteConfig,
   defineConfig({
     test: {
-      projects: [
-        {
-          test: {
-            name: "client",
-            // include vite globals for terser test code
-            globals: true,
-            include: ["src/**/*.test.ts"],
-          },
-        },
-      ],
+      environment: "jsdom",
+      globals: true,
+      include: ["src/**/*.test.ts"],
     },
   }),
 );
