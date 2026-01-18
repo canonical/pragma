7ec223bfc5e5880902cf2ca7bb9157260b7a05ef
advl <700@779.mx>
2026-01-18 15:57:40 +0100

    Updated internal import paths

--- packages/storybook/addon-baseline-grid/src/manager.tsx
+++ packages/storybook/addon-baseline-grid/src/manager.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { addons, types } from "storybook/internal/manager-api";
+import { addons, types } from "storybook/manager-api";
 
 import { Tool } from "./components/Tool.js";
 import { ADDON_ID, TOOL_ID } from "./constants.js";
