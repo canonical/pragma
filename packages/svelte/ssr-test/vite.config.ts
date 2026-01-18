6f48edf2f6d9dae374eb0274ab363c40bdf0ee85
advl <700@779.mx>
2026-01-18 17:07:36 +0100

    -

--- packages/svelte/ssr-test/vite.config.ts
+++ packages/svelte/ssr-test/vite.config.ts
@@ -1,10 +1,17 @@
 /// <reference types="vitest/config" />
 
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { svelte, vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [process.env.VITEST ? svelte() : null].filter(Boolean),
+  plugins: [
+    process.env.VITEST
+      ? svelte({
+          configFile: false,
+          preprocess: vitePreprocess(),
+        })
+      : null,
+  ].filter(Boolean),
   build: {
     sourcemap: true,
   },
