c56f2d687a5f5fe1bd6c67fc2a191df528c74036
advl <700@779.mx>
2026-01-20 21:38:50 +0100

    Added tests for helpers

--- packages/storybook/helpers/vitest.setup.ts
+++ packages/storybook/helpers/vitest.setup.ts
@@ -2,7 +2,7 @@ import * as matchers from "@testing-library/jest-dom/matchers";
 import { cleanup } from "@testing-library/react";
 import { afterEach, expect } from "vitest";
 
-expect.extend(matchers as Parameters<typeof expect.extend>[0]);
+expect.extend(matchers as unknown as Parameters<typeof expect.extend>[0]);
 
 afterEach(() => {
   cleanup();
