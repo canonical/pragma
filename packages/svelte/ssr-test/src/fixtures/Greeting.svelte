37583a16e7a6692e17ae632886630342547a5947
Goulin Khoge <36013798+goulinkh@users.noreply.github.com>
2026-02-04 15:00:18 +0100

    feat(ds-app-launchpad): bootstrap Svelte project (#399)
    * feat(ds-app-launchpad): bootstrap Svelte project
    
    - Introduced the @canonical/svelte-ds-app-launchpad package with Svelte components for the Launchpad application.
    - Added TypeScript and Vite configurations, along with a biome configuration for linting.
    - Included an Example component with SSR and unit tests.
    - Updated .gitignore to exclude test screenshots.
    
    * feat(actions): add caching and installation steps for Playwright browsers
    
    * fix: remove unused dependencies
    
    * refactor(playwright): update Playwright setup to only svelte package and add experimental flag
    
    * feat(biome): add new biome configuration and update package references
    
    * feat(webarchitect): add package-svelte ruleset and update launchpad scripts
    
    * fix(svelte/ssr-test): fix biome check
    
    * feat(biome-svelte): introduce Svelte-specific Biome configuration
    
    * docs(README): update testing instructions and add note about Playwright requirement

--- packages/svelte/ssr-test/src/fixtures/Greeting.svelte
+++ packages/svelte/ssr-test/src/fixtures/Greeting.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-// biome-ignore lint/correctness/noUnusedVariables: false alarm
-const { name }: { name: string } = $props();
+  const { name }: { name: string } = $props();
 </script>
 
 <div class="test-class">
--- packages/svelte/ssr-test/vitest.config.ts
+++ packages/svelte/ssr-test/vitest.config.ts
@@ -2,11 +2,11 @@ import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "./vite.config.ts";
 
 export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: "node",
-      include: ["src/**/*.tests.ts"],
-    },
-  }),
+	viteConfig,
+	defineConfig({
+		test: {
+			environment: "node",
+			include: ["src/**/*.tests.ts"],
+		},
+	}),
 );
