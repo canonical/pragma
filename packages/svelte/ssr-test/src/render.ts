4c9177185e3c7c075801fa25222aebecfda01e9b
steciuk <a.t.steciuk@gmail.com>
2026-02-09 12:49:16 +0100

    feat(svelte-ssr-test): Remove legacy component types

--- packages/svelte/ssr-test/src/render.ts
+++ packages/svelte/ssr-test/src/render.ts
@@ -1,9 +1,9 @@
 import { getQueriesForElement, prettyDOM } from "@testing-library/dom";
 import { JSDOM } from "jsdom";
 import { render as renderSvelte } from "svelte/server";
-import type { RenderResult, RenderSvelteFn } from "./types.js";
+import type { RenderFn } from "./types.js";
 
-const render = ((...args): ReturnType<RenderSvelteFn> & RenderResult => {
+const render: RenderFn = (...args) => {
   // biome-ignore lint/suspicious/noExplicitAny: silence Svelte type inference
   const svelteRenderResult = renderSvelte(...(args as [any]));
   const { window } = new JSDOM(
@@ -21,6 +21,6 @@ const render = ((...args): ReturnType<RenderSvelteFn> & RenderResult => {
     container,
     pretty: (maxLength = 10000) => prettyDOM(container, maxLength) || "",
   };
-}) satisfies RenderSvelteFn;
+};
 
 export default render;
