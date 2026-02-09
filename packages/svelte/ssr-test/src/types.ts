0c68cf9f7f11e79faeecf8233eef3b2383d9a8e4
Adam Steciuk <adam.steciuk@canonical.com>
2026-02-09 18:34:35 +0100

    feat(svelte-ssr-test): Remove legacy component types (#416)

--- packages/svelte/ssr-test/src/types.ts
+++ packages/svelte/ssr-test/src/types.ts
@@ -1,8 +1,7 @@
 import type { BoundFunctions, queries } from "@testing-library/dom";
 import type { DOMWindow } from "jsdom";
+import type { Component, ComponentProps } from "svelte";
 import type { render as renderSvelte } from "svelte/server";
-
-export type RenderSvelteFn = typeof renderSvelte;
 
 type Queries = BoundFunctions<typeof queries>;
 
@@ -12,3 +11,33 @@ export type RenderResult = Queries & {
   container: HTMLElement;
   pretty: (maxLength?: number) => string;
 };
+
+// Types based on Svelte's render function, with the problematic typing for Svelte 4 legacy components removed. This can be removed once the legacy component typing is removed from Svelte itself.
+export type RenderFn = <
+  // biome-ignore lint/suspicious/noExplicitAny: copied from Svelte types
+  Comp extends Component<any>,
+  Props extends ComponentProps<Comp> = ComponentProps<Comp>,
+>(
+  // biome-ignore lint/complexity/noBannedTypes: copied from Svelte types
+  ...args: {} extends Props
+    ? [
+        component: Comp,
+        options?: {
+          props?: Props;
+          // biome-ignore lint/suspicious/noExplicitAny: copied from Svelte types
+          context?: Map<any, any>;
+          idPrefix?: string;
+          csp?: { nonce?: string; hash?: boolean };
+        },
+      ]
+    : [
+        component: Comp,
+        options: {
+          props: Props;
+          // biome-ignore lint/suspicious/noExplicitAny: copied from Svelte types
+          context?: Map<any, any>;
+          idPrefix?: string;
+          csp?: { nonce?: string; hash?: boolean };
+        },
+      ]
+) => ReturnType<typeof renderSvelte> & RenderResult;
