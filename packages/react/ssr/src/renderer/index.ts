5587581b71a1cc84f385c08c3e3effb6e38a3ccc
Alvaro Mateo <alvaromateo9@gmail.com>
2026-02-10 15:55:54 +0100

    Fix PR comments

--- packages/react/ssr/src/renderer/index.ts
+++ packages/react/ssr/src/renderer/index.ts
@@ -1,8 +1,6 @@
-export { default as BaseRenderer } from "./BaseRenderer.js";
 export * from "./constants.js";
 export { default as Extractor } from "./Extractor.js";
-export { default as PipeableStreamRenderer } from "./PipeableStreamRenderer.js";
-export { default as StringRenderer } from "./StringRenderer.js";
+export { default as JSXRenderer } from "./JSXRenderer.js";
 export type {
   RendererOptions,
   RenderHandler,
