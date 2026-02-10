082b550da2ef94a90506950528096e4dd44364e1
advl <700@779.mx>
2026-02-10 15:50:47 +0100

    More phantom deps fixing, linting

--- apps/react/demo/.storybook/preview.ts
+++ apps/react/demo/.storybook/preview.ts
@@ -1,24 +1,10 @@
-import { withThemeByClassName } from "@storybook/addon-themes";
-import type { Preview, ReactRenderer } from "@storybook/react-vite";
+import { previewConfig } from "@canonical/storybook-config/preview";
+import type { Preview } from "@storybook/react-vite";
 
 import "index.css";
 
 const preview: Preview = {
-  decorators: [
-    withThemeByClassName<ReactRenderer>({
-      themes: {
-        light: "is-light",
-        dark: "is-dark",
-        paper: "is-paper",
-      },
-      defaultTheme: "light",
-    }),
-  ],
-  parameters: {
-    docs: {
-      codePanel: true,
-    },
-  },
+  ...previewConfig,
 };
 
 export default preview;
