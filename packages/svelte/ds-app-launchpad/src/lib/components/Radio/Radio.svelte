6c0c7293184177cf7f29e167cfdbe5e78b147352
Goulin Khoge <36013798+goulinkh@users.noreply.github.com>
2026-02-24 20:10:33 +0100

    feat(svelte-ds-app-launchpad): upstream Radio component

---
+++ packages/svelte/ds-app-launchpad/src/lib/components/Radio/Radio.svelte
@@ -0,0 +1,69 @@
+<script lang="ts" generics="T">
+  import type { RadioProps } from "./types.js";
+  import "./styles.css";
+
+  const componentCssClassName = "ds radio";
+
+  let {
+    class: className,
+    group = $bindable(),
+    onchange: onchangeProp,
+    value,
+    ...rest
+  }: RadioProps<T> = $props();
+
+  const onchange: typeof onchangeProp = (e) => {
+    onchangeProp?.(e);
+    if (value && (e.target as HTMLInputElement).checked) {
+      group = value;
+    }
+  };
+</script>
+
+<input
+  type="radio"
+  class={[componentCssClassName, className]}
+  {onchange}
+  {value}
+  checked={value !== undefined && group === value ? true : undefined}
+  {...rest}
+/>
+
+<!-- @component
+`Radio` is a input control for selecting a single option from a set.
+
+As an input control, it requires a `<label>` associated with it.
+
+## Example Usage
+```svelte
+<label>
+  <Radio checked name="theme" value="light" />
+  Light
+</label>
+<label>
+  <Radio name="theme" value="dark" />
+  Dark
+</label>
+```
+
+## Group Control
+The component supports `bind:group` for controlling a group of radios, similarly to [native Svelte bind:group](https://svelte.dev/docs/svelte/bind#input-bind:group).
+
+If `bind:group` is used, the `checked` prop must be omitted, and each radio in the group must have a `value` prop set.
+
+### Example Usage
+```svelte
+<script lang="ts">
+  let theme = $state("light");
+</script>
+
+<label>
+  <Radio bind:group={theme} value="light" />
+  Light
+</label>
+<label>
+  <Radio bind:group={theme} value="dark" />
+  Dark
+</label>
+```
+-->
---
+++ packages/svelte/ds-app-launchpad/src/lib/components/Radio/types.ts
@@ -0,0 +1,22 @@
+import type { HTMLInputAttributes } from "svelte/elements";
+
+interface BaseProps
+  extends Omit<HTMLInputAttributes, "children" | "indeterminate"> {
+  type?: "radio";
+}
+
+export interface CheckedControlledRadioProps<T> extends BaseProps {
+  value?: T;
+  group?: never;
+  checked?: boolean;
+}
+
+export interface GroupControlledRadioProps<T> extends BaseProps {
+  value: T;
+  group: T;
+  checked?: never;
+}
+
+export type RadioProps<T = BaseProps["value"]> =
+  | CheckedControlledRadioProps<T>
+  | GroupControlledRadioProps<T>;
