2a06946579083a909fb3e87701bc17ff5206c920
Goulin Khoge <36013798+goulinkh@users.noreply.github.com>
2026-02-26 10:44:57 +0100

    fix(svelte-ds-app-launchpad): update Vite configuration and add client setup file

---
+++ packages/svelte/ds-app-launchpad/vitest-setup-client.ts
@@ -0,0 +1,9 @@
+/// <reference types="@vitest/browser/matchers" />
+
+import "@canonical/launchpad-design-tokens/dist/css/dimension/responsive.css";
+import "@canonical/launchpad-design-tokens/dist/css/typography/responsive.css";
+import "@canonical/launchpad-design-tokens/dist/css/opacity/opacity.css";
+import "@canonical/launchpad-design-tokens/dist/css/transition/preferred.css";
+import "@canonical/launchpad-design-tokens/dist/css/color/light.css";
+import "./src/lib/index.css";
+
