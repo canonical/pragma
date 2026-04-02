0c811ed55ddcf2348ab67e777e87440c25ff51fb
steciuk <a.t.steciuk@gmail.com>
2026-04-02 13:04:03 +0200

    feat(svelte-ds-app-launchpad): Add Modal component

---
+++ packages/svelte/ds-app-launchpad/src/lib/components/Modal/stories/Modal.Content.stories.svelte
@@ -0,0 +1,28 @@
+<script module lang="ts">
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import { Button } from "../../Button/index.js";
+  import { Modal } from "../index.js";
+
+  const { Story } = defineMeta({
+    title: "Components/Modal/Content",
+    tags: ["autodocs"],
+    component: Modal.Content,
+  });
+</script>
+
+<Story name="Default" asChild>
+  <Modal.Content>
+    <Modal.Content.Header>
+      Discard pending review?
+      <Modal.Content.Header.CloseButton />
+    </Modal.Content.Header>
+    <Modal.Content.Body>
+      You have added 4 comments. Discarding the pending review will permanently
+      delete them. Are you sure you want to continue?
+    </Modal.Content.Body>
+    <Modal.Content.Footer>
+      <Button>Keep review</Button>
+      <Button severity="negative">Discard review</Button>
+    </Modal.Content.Footer>
+  </Modal.Content>
+</Story>
---
+++ packages/svelte/ds-app-launchpad/src/lib/components/Modal/test.fixtures.svelte
@@ -0,0 +1,23 @@
+<script lang="ts" module>
+  import type { ModalTriggerProps } from "./types.js";
+
+  const triggerText = "Open Modal";
+  const contentText = "This is the modal content.";
+  const closeButtonText = "Close";
+
+  // biome-ignore lint/style/useExportType: False positive, presumably due to "cross-language" export
+  export { children, closeButtonText, contentText, trigger, triggerText };
+</script>
+
+{#snippet trigger(triggerProps: ModalTriggerProps)}
+  <button {...triggerProps}>{triggerText}</button>
+{/snippet}
+
+{#snippet children(commandfor: string, close: () => void)}
+  <div>
+    <p>{contentText}</p>
+    <button {commandfor} command="close" onclick={close}
+      >{closeButtonText}</button
+    >
+  </div>
+{/snippet}
