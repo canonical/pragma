e760458b4109be0fb68d11759a2f453d143c72a3
Muhammad Ali <its.muhammad.ali.mughal@gmail.com>
2026-04-15 14:59:15 +0500

    feat(List, BasicSection): Implement List and BasicSection webcomponents (#583)
    * feat: implement BasicSection web component in lit
    
    * feat: Implement List webcomponent using Lit
    
    * feat: add contentBlocks to BasicSection
    
    * address comments

--- packages/lit/ds-prototype/src/lib/index.ts
+++ packages/lit/ds-prototype/src/lib/index.ts
@@ -1,5 +1,7 @@
+export * from "./BasicSection/index.js";
 export * from "./Button/index.js";
 export * from "./Example/index.js";
 export * from "./Hero/index.js";
+export * from "./List/index.js";
 export * from "./SiteLayout/index.js";
 export * from "./TieredList/index.js";
