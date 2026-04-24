c7964513341763d1614635ff3f0a1c3f40eb0495
Adam Steciuk <adam.steciuk@canonical.com>
2026-04-24 15:03:49 +0200

    feat(svelte-ds-app-launchpad): Add SidePanel (#590)
    * feat(svelte-ds-app-launchpad): Add SidePanel component

---
+++ packages/svelte/ds-app-launchpad/src/lib/components/SidePanel/stories/SidePanel.Content.CloseButton.stories.svelte
@@ -0,0 +1,14 @@
+<script module lang="ts">
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import { SidePanel } from "../index.js";
+
+  const { Story } = defineMeta({
+    title: "Components/SidePanel/Content/Header/CloseButton",
+    tags: ["autodocs"],
+    component: SidePanel.Content.Header.CloseButton,
+  });
+</script>
+
+<Story name="Dismiss" asChild>
+  <SidePanel.Content.Header.CloseButton />
+</Story>
---
+++ packages/svelte/ds-app-launchpad/src/lib/components/SidePanel/test.fixtures.svelte
@@ -0,0 +1,23 @@
+<script lang="ts" module>
+  import type { SidePanelTriggerProps } from "./types.js";
+
+  const triggerText = "Open SidePanel";
+  const contentText = "This is the side panel content.";
+  const closeButtonText = "Close";
+
+  // biome-ignore lint/style/useExportType: False positive, presumably due to "cross-language" export
+  export { children, closeButtonText, contentText, trigger, triggerText };
+</script>
+
+{#snippet trigger(triggerProps: SidePanelTriggerProps)}
+  <button {...triggerProps}>{triggerText}</button>
+{/snippet}
+
+{#snippet children(commandfor: string, close: () => void)}
+  <div>
+    <p>{contentText}</p>
+    <button {commandfor} command="close" onclick={close}
+      >{closeButtonText}</button
+    >
+  </div>
+{/snippet}
