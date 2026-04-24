e4640bb720f01235a69b5b4ef1f92fd8f9a0dd86
steciuk <a.t.steciuk@gmail.com>
2026-04-24 15:50:11 +0200

    claim a TODO in icons

--- packages/svelte/ds-app-launchpad/src/lib/components/icons/index.ts
+++ packages/svelte/ds-app-launchpad/src/lib/components/icons/index.ts
@@ -1,2 +1,2 @@
-// TODO: Remove icons when they are approved, upstreamed to `@canonical/ds-assets` included in `@canonical/svelte-icons`.
+// TODO(steciuk): Remove icons when they are approved, upstreamed to `@canonical/ds-assets` included in `@canonical/svelte-icons`.
 export { default as SortIcon } from "./SortIcon.svelte";
