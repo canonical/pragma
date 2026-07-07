bb15f06aa72302b541cac5a697a8c341e9a51b15
Julie Muzina <julie.muzina@canonical.com>
2026-07-07 14:14:59 -0400

    adjust barrel exports to align with react

--- packages/svelte/ds-global/src/lib/index.ts
+++ packages/svelte/ds-global/src/lib/index.ts
@@ -5,4 +5,9 @@
  * This package provides foundational Svelte UI components for Canonical applications.
  */
 
-export * from "./components/index.js";
+export * from "./_work_in_progress/index.js";
+export * from "./component/index.js";
+export * from "./group/index.js";
+export * from "./pattern/index.js";
+export * from "./subcomponent/index.js";
+export type * from "./types/index.js";
