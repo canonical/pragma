e0564dc5d4056dc31c85bda251c95145f86c9148
advl <700@779.mx>
2026-07-21 10:34:28 +0200

    chore(pragma-docs): scaffold JourneyTable via the summon generator (AV-351)

--- apps/react/pragma-docs/src/domains/lenses/journeys/index.ts
+++ apps/react/pragma-docs/src/domains/lenses/journeys/index.ts
@@ -3,3 +3,4 @@ export * from "./JourneyRail/index.js";
 export * from "./JourneysExplorer/index.js";
 export * from "./JourneysPage/index.js";
 export * from "./JourneyWell/index.js";
+export * from "./JourneyTable/index.js";
