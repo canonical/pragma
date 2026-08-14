e0123db88a9e960b97a82597d2e75a92671ddb14
Mihaj <michael.kapustey@canonical.com>
2026-08-14 19:56:19 +0200

    Added comments to modifier families

--- packages/svelte/ds-app-launchpad/src/lib/modifier-families/constants.ts
+++ packages/svelte/ds-app-launchpad/src/lib/modifier-families/constants.ts
@@ -1,5 +1,9 @@
 export const MODIFIER_FAMILIES = {
   criticality: ["success", "error", "warning", "information"],
+  // TODO: old modifier family. Remove severity after its no longer used
   severity: ["neutral", "positive", "negative", "caution", "information"],
+  // TODO: new modifier family is also used density, but the values 
+  // are different. Keep in mind when updating a component that uses it
+  // New values: comfortable, dense
   density: ["dense", "compact", "medium"],
 } as const satisfies Record<string, readonly string[]>;
